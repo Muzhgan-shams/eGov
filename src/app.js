6b255d5f3684884a47d0aa52c6d1d3ba18d12a29
Muzhgan-shams <muzhgan_shams@yahoo.com>
2025-10-03 16:23:32 +0500

    CORS Update

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173', credentials: true }));
+// app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173', credentials: true }));
 
 const isProd = process.env.NODE_ENV === 'production';
 
